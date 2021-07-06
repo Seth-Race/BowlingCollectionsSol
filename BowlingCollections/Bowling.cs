d74de3c34a7ef2bf167a555f8c1ab59fc0a945af
Seth-Race <sethrace@gmail.com>
2021-07-06 14:02:47 -0400

    Add project files.

---
+++ BowlingCollections/Bowling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingCollections {
+    class Bowling {
+        List<int> game = new List<int>();
+
+        public int Bowl() {
+            for (var idx = 1; idx <= 10; idx++) {
+                Console.Write($"Enter score for frame {idx}:   ");
+                var score = Convert.ToInt32(Console.ReadLine());
+                game.Add(score);
+            }
+            var total = 0;
+            foreach (var score in game) {
+                total += score;
+            }
+            return total;
+        }
+    
+    }
+
+}
---
+++ BowlingCollections/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BowlingCollections{
+    class Program {
+        static void Main(string[] args) {
+            var game = new Bowling();
+            Console.WriteLine($"Game Score: {game.Bowl()}");
+            
+        }
+    }
+}
