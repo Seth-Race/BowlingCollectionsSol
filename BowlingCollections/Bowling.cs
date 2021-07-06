ed5b12825238b6d15a5d04bef4f09eac864aa892
Seth-Race <sethrace@gmail.com>
2021-07-06 14:08:24 -0400

    changed to have random method put in numbers.

--- BowlingCollections/Bowling.cs
+++ BowlingCollections/Bowling.cs
@@ -4,12 +4,14 @@ using System.Text;
 
 namespace BowlingCollections {
     class Bowling {
+        Random rnd = new Random();
         List<int> game = new List<int>();
 
         public int Bowl() {
             for (var idx = 1; idx <= 10; idx++) {
-                Console.Write($"Enter score for frame {idx}:   ");
-                var score = Convert.ToInt32(Console.ReadLine());
+                var score = rnd.Next(31);
+             //   Console.Write($"Enter score for frame {idx}:   ");
+             //   var score = Convert.ToInt32(Console.ReadLine());
                 game.Add(score);
             }
             var total = 0;
